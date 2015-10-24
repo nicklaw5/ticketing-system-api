1328489c22c06c0d995a47308b7bc91d5491bf49
Nicholas Law <nick@nicholaslaw.com.au>
2015-10-24 01:52:41 +0000

    continued work on api responses and exception throwing

--- app/Stryve/Response/ApiResponses.php
+++ app/Stryve/Response/ApiResponses.php
@@ -7,51 +7,21 @@ use Config;
 class ApiResponses {
 
     // HTTP Response Codes
-    const HTTP_CONTINUE = 100;
-    const HTTP_SWITCHING_PROTOCOLS = 101;
-    const HTTP_PROCESSING = 102;            // RFC2518
     const HTTP_OK = 200;
     const HTTP_CREATED = 201;
     const HTTP_ACCEPTED = 202;
-    const HTTP_NON_AUTHORITATIVE_INFORMATION = 203;
-    const HTTP_NO_CONTENT = 204;
-    const HTTP_RESET_CONTENT = 205;
-    const HTTP_PARTIAL_CONTENT = 206;
-    const HTTP_MULTI_STATUS = 207;          // RFC4918
-    const HTTP_ALREADY_REPORTED = 208;      // RFC5842
-    const HTTP_IM_USED = 226;               // RFC3229
-    const HTTP_MULTIPLE_CHOICES = 300;
-    const HTTP_MOVED_PERMANENTLY = 301;
-    const HTTP_FOUND = 302;
-    const HTTP_SEE_OTHER = 303;
-    const HTTP_NOT_MODIFIED = 304;
-    const HTTP_USE_PROXY = 305;
-    const HTTP_RESERVED = 306;
-    const HTTP_TEMPORARY_REDIRECT = 307;
-    const HTTP_PERMANENTLY_REDIRECT = 308;  // RFC7238
     const HTTP_BAD_REQUEST = 400;
     const HTTP_UNAUTHORIZED = 401;
     const HTTP_PAYMENT_REQUIRED = 402;
     const HTTP_FORBIDDEN = 403;
     const HTTP_NOT_FOUND = 404;
     const HTTP_METHOD_NOT_ALLOWED = 405;
-    const HTTP_NOT_ACCEPTABLE = 406;
-    const HTTP_PROXY_AUTHENTICATION_REQUIRED = 407;
     const HTTP_REQUEST_TIMEOUT = 408;
     const HTTP_CONFLICT = 409;
     const HTTP_GONE = 410;
     const HTTP_LENGTH_REQUIRED = 411;
-    const HTTP_PRECONDITION_FAILED = 412;
-    const HTTP_REQUEST_ENTITY_TOO_LARGE = 413;
-    const HTTP_REQUEST_URI_TOO_LONG = 414;
-    const HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
-    const HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-    const HTTP_EXPECTATION_FAILED = 417;
-    const HTTP_I_AM_A_TEAPOT = 418;                                               // RFC2324
     const HTTP_UNPROCESSABLE_ENTITY = 422;                                        // RFC4918
     const HTTP_LOCKED = 423;                                                      // RFC4918
-    const HTTP_FAILED_DEPENDENCY = 424;                                           // RFC4918
-    const HTTP_RESERVED_FOR_WEBDAV_ADVANCED_COLLECTIONS_EXPIRED_PROPOSAL = 425;   // RFC2817
     const HTTP_UPGRADE_REQUIRED = 426;                                            // RFC2817
     const HTTP_PRECONDITION_REQUIRED = 428;                                       // RFC6585
     const HTTP_TOO_MANY_REQUESTS = 429;                                           // RFC6585
@@ -61,12 +31,6 @@ class ApiResponses {
     const HTTP_BAD_GATEWAY = 502;
     const HTTP_SERVICE_UNAVAILABLE = 503;
     const HTTP_GATEWAY_TIMEOUT = 504;
-    const HTTP_VERSION_NOT_SUPPORTED = 505;
-    const HTTP_VARIANT_ALSO_NEGOTIATES_EXPERIMENTAL = 506;                        // RFC2295
-    const HTTP_INSUFFICIENT_STORAGE = 507;                                        // RFC4918
-    const HTTP_LOOP_DETECTED = 508;                                               // RFC5842
-    const HTTP_NOT_EXTENDED = 510;                                                // RFC2774
-    const HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;                             // RFC6585
 
     // OTHER CONSTANTS
     const ERROR_TEXT = 'error';
@@ -184,13 +148,13 @@ class ApiResponses {
     /**
      * Sets the HTTP response status text.
      *
-     * @param string $statusCode
+     * @param string $text
      *
      * @return self
      */
-    public function setStatusText($statusCode)
+    public function setStatusText($text)
     {
-        $this->statusText = $statusCode;
+        $this->statusText = trim($text);
 
         return $this;
     }
@@ -232,11 +196,11 @@ class ApiResponses {
      * Sets the HTTP response message
      *
      * @param string $msg
-     * @return $this
+     * @return self
      */
     public function setStatusMessage($msg)
     {	
-        $msg = trim((string) $msg);
+        $msg = (string) trim($msg);
 
     	if($msg === '')
     		$this->statusMessage = $this->getStatusMessage();
@@ -260,11 +224,11 @@ class ApiResponses {
      * Sets error message
      * 
      * @param string $msg
-     * @return $this
+     * @return self
      */
     public function setErrorMessage($msg)
     {
-        $msg = trim($msg);
+        $msg = (string) trim($msg);
         $this->errorMessage = ($msg === '')? self::UNKNOWN_ERROR: $msg;
 
         return $this;
@@ -284,11 +248,11 @@ class ApiResponses {
      * Set application error code
      *
      * @param int $code
-     * @return $this
+     * @return self
      */
     public function setErrorCode($code)
     {
-        $this->errorCode = $code;
+        $this->errorCode = (int) $code;
 
         return $this;
     }
